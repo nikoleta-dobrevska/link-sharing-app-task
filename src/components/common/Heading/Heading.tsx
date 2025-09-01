b1eee0a2fbaf1b53d377b9f3078c60d315aa213a
nikoleta-dobrevska <nikoletadobrevska@gmail.com>
2025-09-01 16:04:16 +0300

    style Heading component

--- src/components/common/Heading/Heading.tsx
+++ src/components/common/Heading/Heading.tsx
@@ -1,10 +1,16 @@
+import headingClasses from "./Heading.module.scss";
+
 interface HeadingProps {
   headingType: string;
   title: string;
 }
 
 const Heading = ({ headingType, title }: HeadingProps) => {
-  return headingType === "M" ? <h1>{title}</h1> : <h2>{title}</h2>;
+  return headingType === "M" ? (
+    <h1 className={headingClasses["Heading__type-M"]}>{title}</h1>
+  ) : (
+    <h2 className={headingClasses["Heading__type-S"]}>{title}</h2>
+  );
 };
 
 export default Heading;
