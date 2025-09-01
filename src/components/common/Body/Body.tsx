7b545cce5d003fea1adf8e2fef9e0f7882210a8c
nikoleta-dobrevska <nikoletadobrevska@gmail.com>
2025-09-01 16:24:37 +0300

    add styling to Body component

--- src/components/common/Body/Body.tsx
+++ src/components/common/Body/Body.tsx
@@ -1,10 +1,22 @@
+import bodyClasses from "./Body.module.scss";
+
 interface BodyProps {
   bodyType: string;
   text: string;
 }
 
 const Body = ({ bodyType, text }: BodyProps) => {
-  return <p>{text}</p>;
+  return (
+    <p
+      className={
+        bodyType === "M"
+          ? bodyClasses["Body__type-M"]
+          : bodyClasses["Body__type-S"]
+      }
+    >
+      {text}
+    </p>
+  );
 };
 
 export default Body;
