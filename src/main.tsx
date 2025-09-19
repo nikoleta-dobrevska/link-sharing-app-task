34067e13d1a2af6627318485416ac7535eee6027
nikoleta-dobrevska <nikoletadobrevska@gmail.com>
2025-09-19 13:16:32 +0300

    fix merge conflict

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "@/App";
-
 import "@/scss/reset.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <></>
   </StrictMode>
 );
