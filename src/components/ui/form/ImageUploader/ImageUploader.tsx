3cd8b4769ccdf31a823d9b970c2b677ceb38b53a
Nikoleta Dobrevska <nikoletadobrevska@Nikoletas-MacBook-Air.local>
2025-10-01 13:27:22 +0300

    create and style image upload component with preview option for the uploaded image

---
+++ src/components/ui/form/ImageUploader/ImageUploader.tsx
@@ -0,0 +1,53 @@
+import { clsx } from "clsx";
+import { useRef, useState } from "react";
+
+import ImageIcon from "@/assets/svgr/Image.svg?react";
+import { Typography } from "@/components/typography";
+
+import imageUploaderClasses from "./ImageUploader.module.scss";
+
+export const ImageUploader = () => {
+  const [preview, setPreview] = useState<string | undefined>(undefined);
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleImagePreview = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setPreview(URL.createObjectURL(e.target.files[0]));
+    }
+  };
+
+  return (
+    <button
+      className={clsx(
+        imageUploaderClasses["img-uploader"],
+        preview && imageUploaderClasses["img-uploader--has-preview"]
+      )}
+      onClick={() => inputRef?.current?.click()}
+    >
+      <input
+        ref={inputRef}
+        className={imageUploaderClasses["img-uploader__file"]}
+        onChange={handleImagePreview}
+        type="file"
+        accept="image/png, image/jpeg, image/jpg"
+        aria-required="true"
+      />
+      {preview && (
+        <img src={preview} alt="" className={imageUploaderClasses["preview"]} />
+      )}
+      <ImageIcon
+        aria-hidden="true"
+        className={imageUploaderClasses["img-uploader__svg"]}
+      />
+      <div className={imageUploaderClasses["img-uploader__text"]}>
+        {!preview && <span aria-hidden="true">+</span>}
+        <Typography component="span" variant="heading" size="sm">
+          {preview ? "Change Image" : "Upload Image"}
+        </Typography>
+      </div>
+    </button>
+  );
+};
+
+//add id
